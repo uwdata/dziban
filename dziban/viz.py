9e9ac1010a9f30ce0cd488faffc416ee32484c4b
Halden Lin <halden.lin@gmail.com>
2019-01-26 16:47:27 -0800

    basic recommendations

--- dziban/viz.py
+++ dziban/viz.py
@@ -1,13 +1,18 @@
 from typing import List
+from draco.run import run as run_draco
+from draco.js import cql2asp
+from draco.helper import data_to_asp
+import json
 
 from dziban.mark import VizMark
 from dziban.encoding import VizEncoding
 
 class Viz(VizMark, VizEncoding):
-  def __init__(self):
+  def __init__(self, data=None):
     VizMark.__init__(self)
     VizEncoding.__init__(self)
-    
+
+    self._data = data
     self._strict = False
     return
 
@@ -15,7 +20,8 @@ class Viz(VizMark, VizEncoding):
     self._strict = True
     return self
 
-  def build(self):
+
+  def _spec(self):
     spec = {}
     
     partial = self._is_partial()
@@ -27,6 +33,20 @@ class Viz(VizMark, VizEncoding):
       **self.build_encoding(),
       **self.build_mark()
     }
+
+  def _schema(self):
+    json_data = json.loads(self._data.to_json(orient='records'))
+    return data_to_asp(json_data)
+
+  def build(self):
+    if (self._strict):
+      return self._spec()
+    else:
+      spec = self._spec()
+      schema = self._schema()
+      asp = cql2asp(spec) + schema
+      result = run_draco(asp)
+      return result
 
   def _is_partial(self):
     return (
